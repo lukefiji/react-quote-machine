45a73cf1f82fa6eff7d07f434de3d410e484d5aa
Luke Fiji <luke.fiji@gmail.com>
2017-08-27 22:41:18 -0700

    Finished fetching from Design Quotes

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,6 @@
-import React, { Component } from "react";
+import React, {
+  Component
+} from "react";
 import jsonpP from "jsonp-p";
 
 import quoteImgOpen from "../quote-open.svg";
@@ -15,40 +17,63 @@ class App extends Component {
     };
   }
 
-  generateQuote = async () => {
+  generateQuote = async() => {
     const quoteData = await jsonpP(
-      "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=2&_jsonp=jsonp",
-      { name: "jsonp" }
+      "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=2&_jsonp=jsonp", {
+        name: "jsonp"
+      }
     ).promise;
     console.log(await quoteData[0]);
     const quote = await quoteData[0].content.replace(/<\/?[^>]+(>|$)/g, "");
+    const unescapedQuote = unescape(quote)
     const author = await quoteData[0].title;
-    this.setState({ quote, author });
+    this.setState({
+      quote: unescapedQuote,
+      author
+    });
   };
 
   render() {
-    return (
-      <div>
-        <h1 id="quote__header">Design Quotes</h1>
-        <div id="quote__container">
-          <img id="quote__start" src={quoteImgOpen} />
-          <div id="quote__text">
-            <p>
-              {this.state.quote}
-            </p>
-          </div>
-          <img id="quote__end" src={quoteImgClose} />
-        </div>
-        <p id="quote__author">
-          -{this.state.author}
-        </p>
-        <div id="button__container">
-          <button id="button__new" onClick={this.generateQuote}>
-            New Quote
-          </button>
-          <button id="button__tweet">Tweet</button>
-        </div>
-      </div>
+    return ( <
+      div >
+      <
+      h1 id = "quote__header" > Design Quotes < /h1> <
+      div id = "quote__container" >
+      <
+      img id = "quote__start"
+      src = {
+        quoteImgOpen
+      }
+      /> <
+      div id = "quote__text" >
+      <
+      p > {
+        this.state.quote
+      } <
+      /p> < /
+      div > <
+      img id = "quote__end"
+      src = {
+        quoteImgClose
+      }
+      /> < /
+      div > <
+      p id = "quote__author" >
+      -{
+        this.state.author
+      } <
+      /p> <
+      div id = "button__container" >
+      <
+      button id = "button__new"
+      onClick = {
+        this.generateQuote
+      } >
+      New Quote <
+      /button> <
+      button id = "button__tweet" > Tweet < /button> < /
+      div > <
+      /div>
     );
   }
 }
